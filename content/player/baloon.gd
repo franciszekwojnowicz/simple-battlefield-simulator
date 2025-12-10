03ab8e438bb3fbc660e64d70565da67486197ca0
Franciszek Wojnowicz <62144431+franciszekwojnowicz@users.noreply.github.com>
2025-12-10 23:15:33 +0100

    fix: docs

--- content/player/baloon.gd
+++ content/player/baloon.gd
@@ -1,48 +1,59 @@
 extends CharacterBody3D
 
-# Movement speed in meters per second
+## Movement speed in meters per second
 var speed: float = 10.0
-# Jump impulse strength
+## Jump impulse strength
 var jump_velocity: float = 4.5
-# Gravity (default to project setting)
+## Gravity (default to project setting)
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 var cam : Camera3D
 var is_moving = false
 
+# bullet and its directory
 var BULLET = preload("uid://cimw3lovbsfxb")
 var shoot_dir : Vector3
 
 @onready var cannon: Node3D = $Cannon
 @onready var point: Node3D = $Cannon/Point
 
-# force net variables
+## Describe differnce between forces which afect the baloon in up/down direction
 var force_net : float = 0.0 #N
+## Describe the temperature inside the baloon
 @export var t_inside_baloon : float = 290.0 #K  #350
+## Describe the temperature outside the baloon
 @export var t_outside_baloon : float = 290.0 #K
+## Describe baloon mass without the air inside
 @export var baloon_mass : float = 500.0 #kg
+## Describe baloon inside volume in m3
 @export var baloon_volume : float = 5000.0 #m3
+## Describe atmospheric pressure
 @export var preassure : float = 101325 #Pa
+## Describe gas constant for air
 @export var gas_constant : float = 287.0 #J/(kg*K)
 
+# timer variables
 @onready var temperature_loss_timer: Timer = $TemperatureLossTimer
 var timer_on = false
 
-func _ready():
+func _ready() -> void:
 	cam = get_node("CameraPivot/CameraIsometric")  # adjust path if needed
+	add_to_group("baloon")
 
 
 func _physics_process(delta: float) -> void:
 	if not cam:
 		return
 	moving(delta)
+	# apply cannon rotation towards the current camera view
 	cannon.global_rotation.x = - cam.global_rotation.x - deg_to_rad(40)
 	cannon.global_rotation.y =   cam.global_rotation.y + deg_to_rad(180)
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
-	
-	
-func moving(delta) -> void:
 
+
+## Takes intput from keybord and transform it into baloon movement
+func moving(delta) -> void:
 	# Get input direction 
 	var input_vector = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	# Get Camera direction
@@ -81,7 +92,7 @@ func moving(delta) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 	
-	
+	# calculate baloon net velocity
 	force_net = (gravity * baloon_volume * (preassure/gas_constant) * (1/t_outside_baloon - 1/t_inside_baloon) - baloon_mass * gravity) 
 	velocity.y = force_net / (baloon_mass + baloon_volume*(preassure/(gas_constant*t_inside_baloon)))
 	
@@ -99,8 +110,8 @@ func moving(delta) -> void:
 
 
 
-
-func shoot():
+## Create new instance of bullet scene and add a cannon's direction to the bullet velocity
+func shoot() -> void:
 	var bullet = BULLET.instantiate()
 	# Assign your direction
 	shoot_dir = - cannon.global_position + point.global_position
